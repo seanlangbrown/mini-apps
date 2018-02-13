15375b02c7fe458136cfea1e09190599c081c130
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-13 11:45:39 -0800

    implemented express router

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
 
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
+})
+
+app.listen(3000, () => (console.log('Server live: http://127.0.0.1:3000')));
 
