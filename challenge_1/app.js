6605bbe21a69fe6fda31089fb2d84bb13c376a88
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-12 17:32:16 -0800

    debug X, O object implementation

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -12,7 +12,7 @@ O.val = 'O';
 X.wins = 0;
 O.wins = 0;
 
-var firstTurn = X.val;
+var firstTurn = X;
 
 var movesAllowed;
 
@@ -20,10 +20,10 @@ var movesAllowed;
 var turn;
 
 var toggleTurn = function() {
-  if (turn === X.val) {
-    turn = O.val;
+  if (turn === X) {
+    turn = O;
   } else {
-    turn = X.val;
+    turn = X;
   }
 };
 
@@ -32,11 +32,12 @@ var diagSum = function(piece) {
   var sum = 0;
   piece = piece ? piece : turn;
   //console.log('checking diagSum', piece);
+  console.log('piece.val', piece.val);
   for(var i = 0; i < board.length; i++) {
     //console.log('checking', i);
-    sum += +(board[i][i] === piece);
+    sum += +(board[i][i] === piece.val); //broken
   }
-  //console.log('diagsum', piece, sum);
+  console.log('diagsum', piece.val, sum);
   return sum;
 };
 
@@ -44,8 +45,9 @@ var colSum = function(col, piece) {
   var sum = 0;
   piece = piece ? piece : turn;
   for(var i = 0; i < board.length; i++) {
-    sum += +(board[i][col] === piece);
+    sum += +(board[i][col] === piece.val);
   }
+  console.log('colsum', piece.val, sum);
   return sum;
 };
 
@@ -53,7 +55,7 @@ var rowSum = function(row, piece) {
   piece = piece ? piece : turn;
   var sum = 0;
   for(var i = 0; i < board[row].length; i++) {
-    sum += +(board[row][i] === piece);
+    sum += +(board[row][i] === piece.val);
   }
   //console.log(sum);
   return sum;
@@ -77,12 +79,12 @@ var didWin = function() {
 
 var didTie = function() {
   //console.log('diagsum', diagSum(X.val));
-  if(diagSum(X.val) === 0 || diagSum(O.val) === 0) {
+  if(diagSum(X) === 0 || diagSum(O) === 0) {
     //return false;
   }
   for(var i = 0; i < 3; i++) {
     //console.log('row sums', rowSum(i, X.val), rowSum(i, O.val), colSum(i, X.val), colSum(i, O.val));
-    if (rowSum(i, X.val) === 0 || rowSum(i, O.val) === 0 || colSum(i, X.val) === 0 || colSum(i, O.val) === 0) {
+    if (rowSum(i, X) === 0 || rowSum(i, O) === 0 || colSum(i, X) === 0 || colSum(i, O) === 0) {
       return false;
     }
   }
@@ -96,17 +98,17 @@ var displayMessage = function(message) {
 
 var displayInstructions = function() {
   //console.log('It\'s ' + turn + '\'s turn! Click to place a piece.');
-  displayMessage('It\'s ' + turn + '\'s turn! Click to place a piece.');
+  displayMessage('It\'s ' + turn.val + '\'s turn! Click to place a piece.');
 };
 
 var displayProhibitedMove = function() {
-  displayMessage(turn + ', please choose an empty square');
+  displayMessage(turn.val + ', please choose an empty square');
 }
 
 var addPiece = function(row, col) {
   if(!board[row][col]) {
     //add piece to model
-    board[row][col] = turn;
+    board[row][col] = turn.val;
     //display a piece on the board
     displayPiece(row, col);
     return true;
@@ -120,7 +122,7 @@ var displayPiece = function(row, col) {
   //add a piece to the DOM
   var id = row + ',' + col;
   //console.log(id);
-  document.getElementById(id).innerText = turn;
+  document.getElementById(id).innerText = turn.val;
 };
 
 
@@ -155,7 +157,7 @@ var playMove = function(row, col) {
   //check for win
   if(didWin()) {
     //display win message
-    displayMessage(turn + ' won!');
+    displayMessage(turn.val + ' won!');
     firstTurn = turn;
     movesAllowed = false;
   } else if(didTie()) {
