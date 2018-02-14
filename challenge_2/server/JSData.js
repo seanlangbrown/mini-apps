4933a250038c176ddd764cbba7009c6e1aea3ed0
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-13 20:26:45 -0800

    debug es6 class implementation of JSData

--- challenge_2/server/JSData.js
+++ challenge_2/server/JSData.js
@@ -1,121 +1,124 @@
 
 const _ = require('underscore');
 
-const Data = function() {
-  this._data = [];
-  this._headers = [];
-
-};
-
-const Element = function() {
-  this.value = null;
-  this.head = null;
-  this.row = null;
-};
-
-Data.prototype.addRow = function(row) {
-  if(Array.isArray(row) && row.length === data.length) {
-    this._data.push(row);
+const Data  = class {
+  constructor() {
+    this._data = [];
+    this._headers = [];
   }
-};
 
-Data.prototype.addCol = function(row, header, overwrite=true) {
-  if(header) {
-    let idx = this._headers.indexOf(header);
-    if(overwrite && idx !== -1) {
-      this.iterCol(idx, (item, i) => (item.value = row[i]));
+  addRow (row) {
+    if(Array.isArray(row) && row.length === data.length) {
+      this._data.push(row);
+    }
+  }
+
+  addCol (row, header, overwrite=true) {
+    if(header) {
+      let idx = this._headers.indexOf(header);
+      if(overwrite && idx !== -1) {
+        this.iterCol(idx, (item, i) => (item.value = row[i]));
+      } else {
+        this._headers.push(header);
+        this.iterCol(this._headers.length, (item, i) => {
+          item.value = row[i]
+          return item;
+        });
+      }
+      return true;
     } else {
-      this._headers.push(header);
-      this.iterCol(this._headers.length, (item, i) => {
-        item.value = row[i]
-        return item;
+      this.iterCol(this.headers.length, (item, i) => {
+        if (i === 0) {
+          this._headers.push(row[i]);
+        } else {
+          item.value = row[i];
+          return item;
+        }
+      });
+      return true;
+    }
+    return false;
+  }
+
+
+  iterCol (colnum, callback) {
+    if(colnum < this._headers.length) {
+      _.each(this._data, (row, i) => {
+        callback(row[colnum]);
+      })
+    } else {
+      _.each(this._data, (row, i) => {
+        let newItem = callBack(new Element(), i);
+        if(newItem instanceof Element) {
+          row[colnum] = newItem;
+        }
       });
     }
-    return true;
-  } else {
-    this.iterCol(this.headers.length, (item, i) => {
-      if (i === 0) {
-        this._headers.push(row[i]);
-      } else {
-        item.value = row[i];
-        return item;
-      }
-    });
-    return true;
   }
-  return false;
-}
 
+  iterRow (rownum, callback) {
+    if (row < this._data.length) {
+      _.each(this._data[rownum], callback);
+    }
+  }
 
-Data.prototype.iterCol = function(colnum, callback) {
-  if(colnum < this._headers.length) {
-    _.each(this._data, (row, i) => {
-      callback(row[colnum]);
+  iterAll (callback, headers) {
+    if (headers) {
+      _.each(this._headers, callback);
+    }
+    _.each(this_.data, (row) => {
+      _.each(row, callback);
     })
-  } else {
-    _.each(this._data, (row, i) => {
-      let newItem = callBack(new Element(), i);
-      if(newItem instanceof Element) {
-        row[colnum] = newItem;
-      }
-    });
   }
-};
-
-Data.prototype.iterRow = function(rownum, callback) {
-  if (row < this._data.length) {
-    _.each(this._data[rownum], callback);
+ 
+  iterColValues (colunm, callback) {
+    this.itercol(colnum, (element) => {
+      return callback(element.value);
+    })
   }
-}
 
-Data.prototype.iterAll = function(callback, headers) {
-  if (headers) {
-    _.each(this._headers, callback);
+  iterRowValues (rownum, callback) {
+    this.iterRow(rownum, (element) => {
+      callback(element.value);
+    })
   }
-  _.each(this_.data, (row) => {
-    _.each(row, callback);
-  })
-};
 
-Data.prototype.iterColValues = function(colunm, callback) {
-  this.itercol(colnum, (element) => {
-    return callback(element.value);
-  })
-};
-
-Data.prototype.iterRowValues = function(rownum, callback) {
-  this.iterRow(rownum, (element) => {
-    callback(element.value);
-  })
-};
-
-Data.prototype.iterAllValues = function(callback, headers) {
-  this.iterAll((item) => {
-    callback(item.value);
-  }, headers);
-};
+  iterAllValues (callback, headers) {
+    this.iterAll((item) => {
+      callback(item.value);
+    }, headers);
+  }
 
 //method to insert header,row....
 //method to get header,row...
 
-Data.prototype.printData = function(rowSeperator, colSeperator) {
-  let rows = [];
-  for(var i = 0; i < this._data.length; i++) {
-    let row = []
-    this.iterRowValues(i, (val) => (rows[i].push(val)));
-    rows.push(row.join(colSeperator));
-  };
+  printData (rowSeperator, colSeperator) {
+    let rows = [];
+    for(var i = 0; i < this._data.length; i++) {
+      let row = []
+      this.iterRowValues(i, (val) => (rows[i].push(val)));
+      rows.push(row.join(colSeperator));
+    }
 
-  page = rows.join(rowSeperator);
-  return page;
+    page = rows.join(rowSeperator);
+    return page;
+  }
+
+  printCSV () {
+    return this.printData('\n', ',');
+  }
+
+  importJSON (obj) {
+
+  }
 };
 
-Data.prototype.printCSV = function() {
-  return this.printData('\n', ',');
-};
-
-Data.prototype.importJSON = function(obj) {
-
+class Element {
+  constructor() {
+    this.value = null;
+    this.head = null;
+    this.row = null;
+  }
 };
 
 
