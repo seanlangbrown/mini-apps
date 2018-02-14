1c226d2d5477b53b03c99a47ee078180408f641f
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-14 15:40:08 -0800

    implement connectFour.jsx class

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,10 @@
 //A react connect4 app.
 import React from 'react';
 import ReactDOM  from 'react-dom';
-//ReactDOM.render(ConnectFour, document.getElementById('app'));
+import ConnectFour from './connectFour.jsx';
+ReactDOM.render(<ConnectFour />, document.getElementById('app'));
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('app')
+// );
---
+++ challenge_3/client/connectFour.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class ConnectFour extends React.Component {
+  //console.log('constructor');
+
+
+  render() {
+    console.log('rendering');
+    return ( 
+      <h1>Hello, world two!</h1> 
+    );
+  }
+
+};
+
+export default ConnectFour;
+
+
+
+
+
+
+
+
+
