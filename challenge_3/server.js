0d43279a1e7a8520a933a160113aeb06f23e6bf5
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-14 11:58:10 -0800

    complete express server for index.html and static files

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,19 @@
+//Express server here
+const express = require('express');
+const app = express();
+//server static file for get request
 
+//app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendfile('client/index.html');
+});
+app.get('/app', express.static('client'));
+
+//routing?  Not needed for MVP
+  //later POST win
+  //later GET leaderboard
+
+
+app.listen(3000, ()=>(console.log('Server listening on 127.0.0.1:3000')));
 
