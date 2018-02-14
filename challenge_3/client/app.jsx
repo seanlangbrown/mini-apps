5e7fd8946d9904da6e7ccf21260f9aad6c78ea88
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-14 15:01:58 -0800

    renamed app.js -> app.jsx

---
+++ challenge_3/client/app.jsx
@@ -0,0 +1,9 @@
+//A react connect4 app.
+import React from 'react';
+import ReactDOM  from 'react-dom';
+//ReactDOM.render(ConnectFour, document.getElementById('app'));
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
