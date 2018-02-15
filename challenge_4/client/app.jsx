85f2ef433b33c8356e0d3a1c6c24ed9247cd6271
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-15 14:39:27 -0800

    implement challenge_4 bowlingGame component

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BowlingGame from './bowlingGame.jsx';
 
-//ReactDOM.render(<h1>bowling app</h1>, document.getElementById('app'));
+ReactDOM.render(<BowlingGame />, document.getElementById('app'));
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('app')
+// );
---
+++ challenge_4/client/bowlingGame.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import _ from 'lodash';
+
+
+class BowlingGame extends React.Component {
+	
+
+
+	render () {
+
+
+     return (
+
+       <h1>bowling app</h1> 
+
+
+     );
+
+
+	}
+}
+
+
+export default BowlingGame;
