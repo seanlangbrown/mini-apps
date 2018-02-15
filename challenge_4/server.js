636c90bcce55d5d4db418dbdc540376f7d11648d
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-15 12:15:27 -0800

    complete challeng_4 express server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
 
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/bowling', express.static(__dirname + '/client'));
+
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(__dirname + '/client/dist/bundle.js');
+});
+
+
+app.listen(3000, ()=>(console.log('Server live at 127.0.0.1:3000')));
