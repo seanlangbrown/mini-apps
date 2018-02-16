e4cc99872dd142576b78a3a2b713ecb64c8d3067
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-15 22:12:14 -0800

    implement body parser and additional routes for post/get scores

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const scoreData = require('./server/scoreData.js');
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
@@ -11,6 +15,10 @@ app.use('/bowling', express.static(__dirname + '/client'));
 app.get('/bundle.js', (req, res) => {
   res.sendFile(__dirname + '/client/dist/bundle.js');
 });
+
+app.get('/scores', (req, res) => (scoreData.get(req, res)));
+
+app.post('/scores', (req, res) => (scoreData.post(req, res)));
 
 
 app.listen(3000, ()=>(console.log('Server live at 127.0.0.1:3000')));
